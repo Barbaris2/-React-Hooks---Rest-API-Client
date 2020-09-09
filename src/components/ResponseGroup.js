d3416eee0e3c2368dda32b4fb75e72e35b8fa587
Stolyarov <t9818184233@gmailcom>
2020-09-09 10:52:08 +0300

    response group

--- src/components/ResponseGroup.js
+++ src/components/ResponseGroup.js
@@ -2,10 +2,16 @@ import React, { useContext } from 'react';
 import { ApiContext } from '../context/apiContext/apiContext';
 
 const ResponseGroup = () => {
-  const { apiData } = useContext(ApiContext);
+  const { apiData, apiResponseHeaders } = useContext(ApiContext);
   return (
     <div className='response-group'>
-      <div className='response-status'>
+      <div className='response-headers'>
+        <h6 style={{ marginTop: '10px' }}>Response data:</h6>
+        <div className='overflow-auto bg-light'>
+          <pre>{JSON.stringify(apiResponseHeaders, null, 2)}</pre>
+        </div>
+      </div>
+      <div className='response-data'>
         <h6>Response data:</h6>
         <div className='overflow-auto bg-light'>
           <pre>{JSON.stringify(apiData, null, 2)}</pre>
