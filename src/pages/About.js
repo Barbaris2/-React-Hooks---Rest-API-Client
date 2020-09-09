5ad64bcf45377e37ade5b518c09700432e80095d
Stolyarov <t9818184233@gmailcom>
2020-09-09 16:26:44 +0300

    gi

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,32 @@
 import React from 'react';
 
 export const About = () => {
-  return <h1>About</h1>;
+  return (
+    <div className='jumbotron'>
+      <div className='about-container'>
+        <h2 className='display-4'>Rest API Client</h2>
+        <p className='lead'>
+          &nbsp;&nbsp;&nbsp;Это приложение <strong>React</strong> создано с
+          использованием <strong>Bootstrap</strong> и
+          <strong> React Hooks</strong>.<br />
+          &nbsp;&nbsp;&nbsp;Rest API Client - HTTP-клиент, позволяющий отправить
+          GET, POST, PUT или DELETE запрос на указанный пользователем URL с
+          указанными HTTP-заголовками и телом запроса. Есть валидация введеного
+          URL. Ведется история отправленных запросов и полученых ответов.
+          История сохраняется в localStorage. Можно выполнить старый запрос
+          повторно.
+          <br />
+          &nbsp;&nbsp;&nbsp; Для тестирования запросов использовался сервис{' '}
+          <a
+            href='https://jsonplaceholder.typicode.com/guide.html'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            JSONPlaceholder
+          </a>
+          .
+        </p>
+      </div>
+    </div>
+  );
 };
